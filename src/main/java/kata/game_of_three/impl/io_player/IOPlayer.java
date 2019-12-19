b51d81d9c9c0cd5c3f8ec2571dc6706dda7e37d8
Piercarlo Slavazza <p.slavazza@kie-services.com>
2019-12-19 14:13:42 +0100

    wip

--- src/main/java/kata/game_of_three/impl/io_player/IOPlayer.java
+++ src/main/java/kata/game_of_three/impl/io_player/IOPlayer.java
@@ -2,14 +2,22 @@ package kata.game_of_three.impl.io_player;
 
 import kata.game_of_three.*;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class IOPlayer implements Player {
 
     private final PlayerIdentifier playerIdentifier;
     private final GameTable gameTable;
+    private final Scanner scanner;
+    private final PrintStream printStream;
 
-    public IOPlayer(PlayerIdentifier playerIdentifier, GameTable gameTable) {
+    public IOPlayer(PlayerIdentifier playerIdentifier, GameTable gameTable, InputStream inputStream, PrintStream printStream) {
 	this.playerIdentifier = playerIdentifier;
 	this.gameTable = gameTable;
+	this.scanner = new Scanner(inputStream);
+	this.printStream = printStream;
     }
 
     @Override public PlayerIdentifier getIdentifier() {
@@ -17,7 +25,9 @@ public class IOPlayer implements Player {
     }
 
     @Override public void playTurn(Move opponentMove) {
-
+        printStream.println("Opponent move: " + opponentMove.getResultingNumber());
+        printStream.println("You reply [-1,0,1]:");
+        int reply = scanner.nextInt();
     }
 
     @Override public void endGame(GameResult gameResult) {
