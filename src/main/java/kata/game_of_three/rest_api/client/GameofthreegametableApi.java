523181807e13e79a185c47e728af40225c60e9d5
Piercarlo Slavazza <p.slavazza@kie-services.com>
2019-12-20 11:06:24 +0100

    wip

--- src/main/java/kata/game_of_three/rest_api/client/GameofthreegametableApi.java
+++ src/main/java/kata/game_of_three/rest_api/client/GameofthreegametableApi.java
@@ -164,13 +164,13 @@ public class GameofthreegametableApi {
     Map<String, Object> localVarFormParams = new HashMap<String, Object>();
 
     final String[] localVarAccepts = {
-      
+                    "application/json"
     };
     final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
     if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
 
     final String[] localVarContentTypes = {
-      
+
     };
     final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
     localVarHeaderParams.put("Content-Type", localVarContentType);
