ed35d3d531573d6a754e44bf3b26519a05535263
Piercarlo Slavazza <p.slavazza@kie-services.com>
2019-12-20 10:29:56 +0100

    wip

---
+++ src/main/java/kata/game_of_three/rest_api/client/auth/OAuth.java
@@ -0,0 +1,26 @@
+package kata.game_of_three.rest_api.client.auth;
+
+import kata.game_of_three.rest_api.client.Pair;
+
+import java.util.Map;
+import java.util.List;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2019-12-20T10:25:38.213+01:00")
+public class OAuth implements Authentication {
+  private String accessToken;
+
+  public String getAccessToken() {
+    return accessToken;
+  }
+
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  @Override
+  public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
+    if (accessToken != null) {
+      headerParams.put("Authorization", "Bearer " + accessToken);
+    }
+  }
+}
